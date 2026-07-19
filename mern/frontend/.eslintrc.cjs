242468c5b0af721e761652c37e147365a6b8eb34
dutrei-hailu <dutreihailu@gmail.com>
2026-07-19 23:10:38 +0000

    fix npm run lint issue

--- mern/frontend/.eslintrc.cjs
+++ mern/frontend/.eslintrc.cjs
@@ -1,21 +1,23 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true, vitest: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'cypress'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/prop-types': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+
+  ignorePatterns: ["dist"],
+
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+
+  plugins: [
+    "react",
+  ],
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+};
